b878434eed0ff306aac7419f20e2948aceb9c1c0
johnbanq <johnbanq@gmail.com>
2022-01-16 11:19:14 +0000

    feat: basic welcome window

--- wiresquid/src/main/java/com/github/johnbanq/wiresquid/gui/WelcomeWindow.java
+++ wiresquid/src/main/java/com/github/johnbanq/wiresquid/gui/WelcomeWindow.java
@@ -1,6 +1,8 @@
 package com.github.johnbanq.wiresquid.gui;
 
+import com.github.johnbanq.wiresquid.gui.styling.Fonts;
 import imgui.ImGui;
+import imgui.ImVec2;
 import imgui.type.ImBoolean;
 
 public class WelcomeWindow implements ClosableWindow {
@@ -14,6 +16,10 @@ public class WelcomeWindow implements ClosableWindow {
     public void render() {
         if(shouldShow()) {
             ImGui.begin("Welcome", show);
+            ImGui.text("MC:BE Protocol Inspection & Packet Generation Utility");
+            ImGui.pushFont(Fonts.ITALIC);
+            ImGui.text("\"All hail the flying squid!\"");
+            ImGui.popFont();
             ImGui.end();
         }
     }
